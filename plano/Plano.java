668fb39b8cbf8225fcf2ded98c6af31711644d65
mabn19 <54905903+mabn19@users.noreply.github.com>
2019-09-06 11:30:55 -0500

    Add files via upload

---
+++ plano/Plano.java
@@ -0,0 +1,21 @@
+
+package plano;
+
+public class Plano {
+
+    public static void main(String[] args) {
+    Punto3d punto1 = new Punto3d();
+    Punto3d punto2 = new Punto3d();
+       
+    punto1.setX(4);
+    punto1.setY(10);
+    punto1.setZ(15);
+    punto2.setX(16);
+    punto2.setY(9);
+    punto2.setZ(8);
+    
+    System.out.println(punto1.distancia());    
+    System.out.println(punto2.distancia());
+    }
+    
+}
---
+++ plano/Punto3d.java
@@ -0,0 +1,23 @@
+
+package plano;
+
+
+public class Punto3d extends Punto { 
+    private float z;
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    
+    public double distancia(){
+        return Math.sqrt(Math.pow(this.getX(),2)+Math.pow(this.getY(),2)+Math.pow(this.getZ(),2));
+    }
+    public double distancia(float x, float y, float z){
+        return Math.sqrt(Math.pow(this.getX()-x, 2)+Math.pow(this.getY()-y, 2)+Math.pow(this.getZ()-z, 2));
+    }
+    
+}
