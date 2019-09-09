0a68ed70b588cee2c59038d408d6588e002fb80e
mabn19 <54905903+mabn19@users.noreply.github.com>
2019-09-09 11:39:06 -0500

    Add files via upload

---
+++ calculadoracomplejos/CalculadoraComplejos.java
@@ -0,0 +1,22 @@
+
+package calculadoracomplejos;
+
+
+public class CalculadoraComplejos {
+
+
+    public static void main(String[] args) {
+       Complejo comp1 = new Complejo();
+       Complejo comp2 = new Complejo();
+       
+       comp1.setReal(1);
+       comp1.setImag(2.3f);
+       comp2.setReal(3.4f);
+       comp2.setImag(5.2f);
+       
+       GUI asd = new GUI();
+        
+       
+    }
+    
+}
